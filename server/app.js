b6278e409cc6bfc5109c2b29eeee0be20bf995dd
Ayomide Onalaja <ayo.onalaja@gmail.com>
2019-01-21 15:58:59 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 
 //Connect to mLab database
-mongoose.connect('mongodb://ayo_laja:Omowunmi90@ds161794.mlab.com:61794/graphql-playlist', {useNewUrlParser: true})
+mongoose.connect('mongodb://username:password@ds161794.mlab.com:61794/graphql-playlist', {useNewUrlParser: true})
 //Once coonection is open, fire callback function
 mongoose.connection.once('open', () => {
     console.log('Connection to mongoDB instance on mLab successful')
